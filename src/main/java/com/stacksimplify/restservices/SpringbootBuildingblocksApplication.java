888125f3e8102f06b7d08b68149e751ecf10e8dd
Ridhima <ridhima1999sharma@gmail.com>
2022-04-15 21:16:40 +0530

    second commit

--- src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplication.java
+++ src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplication.java
@@ -2,6 +2,8 @@ package com.stacksimplify.restservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//
 @SpringBootApplication
 public class SpringbootBuildingblocksApplication {
 
