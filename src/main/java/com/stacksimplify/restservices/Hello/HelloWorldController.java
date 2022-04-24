3ddc3873dff9fc025f4e139be6ff9ea028ee6e29
Ridhima <ridhima1999sharma@gmail.com>
2022-04-24 19:39:30 +0530

    Hello World Base

---
+++ src/main/java/com/stacksimplify/restservices/Hello/HelloWorldController.java
@@ -0,0 +1,26 @@
+package com.stacksimplify.restservices.Hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    
+	//@RequestMapping(method= RequestMethod.GET, path="/helloworld")
+	@GetMapping("/helloworld1")
+	public String helloWorld()
+	{
+		return "hello world";
+	}
+	
+	@GetMapping("/helloworldbean")
+	public UserDetails HelloWorldBean()
+	{
+		
+		return new UserDetails("Raghu","Sharma", "Amritsar");
+		
+	}
+	
+}
